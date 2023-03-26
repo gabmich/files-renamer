207ea4d84312c458c11dbcf116c39bef6ea795f1
gabmichelet <gabmichelet@bluewin.ch>
2023-03-26 15:21:15 +0200

    update

--- files_renamer.py
+++ files_renamer.py
@@ -1,29 +1,51 @@
 import os
+from datetime import datetime
 
 
 class RenameFiles():
     """
-    Rename files in a target directory as wanted
+    Rename files in a target directory with file's modification date
     """
     folder_path = ''
     files       = []
+    DATE_FORMAT = '%Y-%m-%d'
 
     def __init__(self, folder_path):
         self.check_folder(folder_path)
 
     def check_folder(self, folder_path):
+        # chef if folder_path exists and ensure that it's a valid directory
         if os.path.exists(str(folder_path)) and os.path.isdir(str(folder_path)):
             self.folder_path = str(folder_path)
             self.get_folder_content()
+            self.list_files()
         else:
             print('not a valid folder')
 
     def get_folder_content(self):
-        self.files = os.listdir(self.folder_path)
-        print('Il y a ', len(self.files), 'fichiers dans ce dossier.')
+        # get files in target folder
+        # excludes all folders and hidden files
+        files_and_dirs  = os.listdir(self.folder_path)
+        self.files      = [f for f in files_and_dirs if (os.path.isfile(f'{self.folder_path}/{f}') and not f.startswith('.'))]
+        print('There are', len(self.files), 'files in this directory.')
+
+    def timestamp_to_datetime(self, file_full_path):
+        # try to transform timestamp to a datestring
+        # date string formatting given in self.FORMAT
+        timestamp = os.path.getmtime(file_full_path)
+
+        try:
+            return datetime.fromtimestamp(timestamp).strftime(self.DATE_FORMAT)
+        except Exception as e:
+            print('Error in the file\'s modification date : not a valid timestamp')
+            return ''
+
+    def list_files(self):
+        for f in self.files:
+            print(f, self.timestamp_to_datetime(f'{self.folder_path}/{f}'))
 
 
+folder_path = input('Enter path of target directory : ')
 
-folder_path = input('Entrez le chemin du dossier cible : ')
-
-a = RenameFiles(folder_path)
+# usage
+RenameFiles(folder_path)
